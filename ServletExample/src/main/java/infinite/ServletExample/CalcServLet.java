37cc290d00d90ca2579b1dee5e85b42e1255ebdf
gummabhargavi <108506501+gummabhargavi@users.noreply.github.com>
2022-09-29 17:01:44 +0530

    Add files via upload

---
+++ ServletExample/src/main/java/infinite/ServletExample/CalcServLet.java
@@ -0,0 +1,49 @@
+package infinite.ServletExample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CalcServLet
+ */
+public class CalcServLet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CalcServLet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int firstNo, secondNo, result;
+		firstNo = Integer.parseInt(request.getParameter("firstNo"));
+		secondNo = Integer.parseInt(request.getParameter("secondNo"));
+		result = firstNo + secondNo;
+		PrintWriter out = response.getWriter();
+		out.println("Sum is " +result+ "<br/>");
+		result = firstNo - secondNo;
+		out.println("Sub is " +result+ "<br/>");
+		result = firstNo * secondNo;
+		out.println("Mul is " +result+ "<br/>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
